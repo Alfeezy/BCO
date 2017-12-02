d79e1358860c23f0c823b70e27123a2a9d3abd3f
Alfeezy <RPGsquirrel@gmail.com>
2017-12-02 16:48:52 -0500

    "map - easy mode"

--- BCO_classes/map.rb
+++ BCO_classes/map.rb
@@ -1,16 +1,26 @@
 class Map
 
-    def initialize(locations, roads)
-        @locations = locations
-        @roads = roads
-    end
+  attr_accessor :locations, :roads, :total_distance
 
-    def ant_storage
-      bus_routes = []
-      @locations.each do |location|
-        location.ants.each do |ant|
-          bus_routes << ant.next_road
-        end
+  def initialize(locations, roads)
+    @locations = locations
+    @roads = roads
+    @total_distance = 0
+  end
+
+  def ant_storage
+    bus_routes = Hash[@roads.map {|x| [x, 0]}]
+    @locations.each do |location|
+      location.ants.each do |ant|
+        bus_routes[ant.next_road.id] += 1
       end
     end
+    bus_routes
+  end
 
+  def bus_distance 
+    ant_storage.each do |key, val|
+      total_distance += (key.distance)*((val+19)/20)
+    end
+  end
+end
